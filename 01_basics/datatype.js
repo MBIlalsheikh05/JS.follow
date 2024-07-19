2cbf7832090f8fc9191234386dc81b5879ca4bd2
MBIlalsheikh05 <140721272+MBIlalsheikh05@users.noreply.github.com>
2024-07-19 18:10:29 +0500

    Add files via upload
    container config

---
+++ 01_basics/datatype.js
@@ -0,0 +1,23 @@
+let name = "Bilal"
+let num = "222"
+let isgoing = true
+let mb // default
+let bi=null // Null is object when we consider its type
+let bilal = Symbol("name") // symbol is use to define any unique things
+let n1 = Number(name)
+
+/*
+When we convert string into number it can't be print actuall velues it will be print NaN => not an number
+"33" => 33
+"Bilal" => NaN
+true => 1, false => 0
+*/
+
+// let n2 = Number(num)
+// console.log(typeof n1)
+// console.log(typeof name)
+// console.log(n1);
+
+let isBool = 0
+let isNewBool = Boolean(isBool)
+console.log(isNewBool);
---
+++ 01_basics/string_operation.js
@@ -0,0 +1,13 @@
+let num = 5
+let num1 = -num
+
+// console.table([num , num1])
+console.log(2-"2");
+console.log(2 * "2");
+
+let n="Hi"
+let name= "Bilal"
+// console.log(n +" "+ name);
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log(1 + "2" + 2);
