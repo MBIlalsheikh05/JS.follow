32ff2d039d430f330c2714cea4ed06de4ab993c4
Muhammad Bilal Sheikh <140721272+MBIlalsheikh05@users.noreply.github.com>
2024-08-11 23:36:52 +0000

    main

---
+++ 05_iteration/highordearry.js
@@ -0,0 +1,32 @@
+const mbs = [1,2,3,4,5,6,7]
+
+// for (const i of mbs) {
+//     console.log(i);   
+// }
+
+const coding = ['js', 'python', 'react', 'Java', 'native']
+
+coding.forEach(( value, index , arr ) =>{
+    // console.log( value, index , arr);
+    
+})
+
+const myarr = [
+    {
+        lang : 'js',
+        lib: 'react'
+    },
+    {
+        lang : 'python',
+        lib: 'django'
+    },
+    {
+        lang : 'PHP',
+        lib: 'Laravel'
+    }
+]
+
+myarr.forEach((item) => {
+    console.log(item.lib);
+    
+})
